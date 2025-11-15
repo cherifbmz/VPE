f4811ad7f5004ce4fb079880cd164a16ec5228fd
Rachidmerabet77 <rachidmerabet77@gmail.com>
2025-11-16 00:06:44 +0100

    Representation ou creation  d'un objet 3D

--- vpe.py
+++ vpe.py
@@ -1,4 +1,23 @@
 import numpy as np
+import math
+
+
+
+class Representation3D:
+    def __init__(self, sommets, aretes):
+        self.sommets = np.array(sommets, dtype=float) 
+        self.aretes = aretes  
+
+cube_sommets = [
+    [-1, -1, -1], [1, -1, -1], [1, 1, -1], [-1, 1, -1],
+    [-1, -1,  1], [1, -1,  1], [1, 1,  1], [-1, 1,  1]
+]
+cube_aretes = [
+    (0,1),(1,2),(2,3),(3,0),
+    (4,5),(5,6),(6,7),(7,4),
+    (0,4),(1,5),(2,6),(3,7)
+]
+CUBE = Representation3D(cube_sommets, cube_aretes)
 
 
 def projection(x,y,z,K):
@@ -17,6 +36,7 @@ def K(f,u0,v0):
 # function de transformation
 def WTC(M, R, t):
     return np.dot(R, M) + t
+
 
 
 
